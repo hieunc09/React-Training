2d0ca2a46672da921a088233b19b270fbeb888ef
HieuNC <peter@balanceinternet.com.au>
2018-12-06 18:26:52 +0700

    update babel, setup webpack and react component

---
+++ indecision-app/src/components/Action.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Action = (props) => {
+    return (
+        <div>
+            <button
+                onClick={props.handlePick}
+                disabled={!props.hasOptions}
+            >
+                What should I do?
+            </button>
+        </div>
+    );
+};
---
+++ indecision-app/src/utils.js
@@ -0,0 +1,7 @@
+
+console.log('Utils is running');
+
+const square = (x) => x * x;
+const add = (a, b) => a + b;
+
+export { square as default, add };
